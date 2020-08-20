bd12d8f9c8a8291a86d228ce4d2bd26e877cbf2d
= <=>
2020-08-20 20:23:51 +0800

    会员服务集成nacos配置中心

--- mall-member/src/main/java/personal/skyxt/mallmember/MallMemberApplication.java
+++ mall-member/src/main/java/personal/skyxt/mallmember/MallMemberApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableDiscoveryClient
 @MapperScan("personal.skyxt.mallmember.dao")
@@ -11,7 +12,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class MallMemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallMemberApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(MallMemberApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :" +userName+"; age: "+userAge);
     }
 
 }
