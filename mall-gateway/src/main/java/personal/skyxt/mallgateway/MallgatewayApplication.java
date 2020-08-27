1c4207d1f022a05c80bb5bddb8a4f2aa199b146a
skyxt <xiangtian.yang@aliyun.com>
2020-08-27 22:26:53 +0800

    gateway服务测试

--- mall-gateway/src/main/java/personal/skyxt/mallgateway/MallgatewayApplication.java
+++ mall-gateway/src/main/java/personal/skyxt/mallgateway/MallgatewayApplication.java
@@ -2,8 +2,11 @@ package personal.skyxt.mallgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MallgatewayApplication {
 
     public static void main(String[] args) {
