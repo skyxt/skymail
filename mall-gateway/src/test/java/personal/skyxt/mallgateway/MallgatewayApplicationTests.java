2b627f3fcbbebf5d5d0122e6346a23e921693978
= <=>
2020-08-27 15:50:31 +0800

    添加网关工程

---
+++ mall-gateway/src/test/java/personal/skyxt/mallgateway/MallgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.skyxt.mallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallgatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
