3e28b3d1aab67243b6d793d9e63c45512429baac
skyxt <xiangtian.yang@aliyun.com>
2020-11-17 23:36:33 +0800

    网关和后台管理系统注册到注册中心

--- mall-admin/src/main/java/personal/skyxt/malladmin/MalladminApplication.java
+++ mall-admin/src/main/java/personal/skyxt/malladmin/MalladminApplication.java
@@ -10,8 +10,10 @@ package personal.skyxt.malladmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MalladminApplication {
 
